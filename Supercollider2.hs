ad9f6076fe392c8e62f58045c87945ac98535445
Paolo Veronelli <paolo.veronelli@gmail.com>
2012-10-04 08:57:15 +0200

    -

--- Supercollider2.hs
+++ Supercollider2.hs
@@ -8,10 +8,11 @@ import Control.Monad
 import Control.Monad.Reader 
 import Control.Concurrent.STMOrIO 
 import Control.Concurrent
-import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM (atomically, newTVarIO, modifyTVar)
 import Sound.SC3 hiding (Binary,select)
 import Sound.OpenSoundControl (OSC (..), Time (..) )
 import System.Environment
+import System.Console.Haskeline
 
 import Control.Arrow (second)
 import Data.Ratio
@@ -21,12 +22,12 @@ import System.Metronome2
 
 cs = withSC3 . flip send 
 
-data Suono = Suono String String
+data Suono = Suono String String deriving (Read,Show)
 
 suona :: RealTime -> Shared String -> [Suono] -> IO ()
 suona (RealTime t) sh xs = let
         each  (Suono name par)  = s_new name (-1) AddToTail 1 . assocs $ findWithDefault empty par sh
-        in cs . Bundle (UTCr (t + 1)) . map each $ xs
+        in cs . Bundle (UTCr (t + 0.2)) . map each $ xs
 
 
 perc :: UGen
@@ -63,21 +64,43 @@ bootSynths = do
         cs . d_recv . synthdef "wow" . out 0 . (\s -> pan2 s 0 1) $ perc * msin
 
 traks = 
-        [       TrackEnv "kick" (TrackCtx 0 0 128) False (RTrack (Action $ Suono "kick" "pk") [0,1%4,2%4,5%8,7%8])
-        ,       TrackEnv "kickA" (TrackCtx 0 0 256) False (RTrack (IS ("pk","amp") 0.8) [0,1%4,2%4,7%8])
-        ,       TrackEnv "kickAl" (TrackCtx 0 0 256) False (RTrack (IS ("pk","amp") 0.5) [0,1%3,2%3])
-        ,       TrackEnv "zill" (TrackCtx 0 0 128) False (RTrack (Action $ Suono "zill" "pz") $ map (*(1%16)) [0..])
+        [       TrackEnv "kick" (TrackCtx 0 0 128) False (RTrack (Action $ Suono "kick" "pk") [0,2%8,6%8])
+        ,       TrackEnv "kick2" (TrackCtx 0 8 32) False (RTrack (Action $ Suono "kick" "pk2") [0,1%2,7%8])
+        ,       TrackEnv "kick2A" (TrackCtx 0 8 32) False (RTrack (IS ("pk2","amp") 0.85) [0])
+        ,       TrackEnv "kick2A1" (TrackCtx 1 8 32) False (RTrack (IS ("pk2","amp") 0.25) [1%2])
+        ,       TrackEnv "kickA" (TrackCtx 0 0 256) False (RTrack (IS ("pk","amp") 0.5) [0,1%4,2%4,7%8])
+        ,       TrackEnv "kickA1" (TrackCtx 0 0 120) False (RTrack (IS ("pk","amp") 0.4) [0,1%4,2%4,7%8])
+        ,       TrackEnv "kickA2" (TrackCtx 0 0 62) False (RTrack (IS ("pk","amp") 0.2) [0,1%4,2%4,7%8])
+        ,       TrackEnv "kickF" (TrackCtx 0 0 256) False (RTrack (IS ("pk","rate") 1.96) [0,1%4,2%4,7%8])
+        ,       TrackEnv "kickF1" (TrackCtx 0 0 120) False (RTrack (IS ("pk","rate") 2.12) [0,1%4,2%4,7%8])
+        ,       TrackEnv "kickF2" (TrackCtx 0 0 62) False (RTrack (IS ("pk","rate") 2) [0,1%4,2%4,7%8])
+        ,       TrackEnv "zill" (TrackCtx 0 0 128) False (RTrack (Action $ Suono "zill" "pz") $ map (*(1%32)) [0..])
         ,       TrackEnv "kickUnphase" (TrackCtx 0 0 256) False (RTrack (MPh "kick" (S 8)) [0,1%3,2%3])
         ,       TrackEnv "kickRephase" (TrackCtx 0 0 512) False (RTrack (MPh "kick" (S (-16))) [0,1%6,5%7])
-        ,       TrackEnv "zillUnphase" (TrackCtx 0 0 128) False (RTrack (MPh "zill" (S 4)) [0,1%3,2%3])
-        ,       TrackEnv "zillRephase" (TrackCtx 0 0 128) False (RTrack (MPh "zill" (S (-4))) [0,1%6,5%7])
+        ,       TrackEnv "kickW" (TrackCtx 0 0 256) False (RTrack (MPe "kick" (I 16)) [0,1%16,5%10])
+        ,       TrackEnv "kickW1" (TrackCtx 0 0 256) False (RTrack (MPe "kick" (I 64)) [0,3%16,13%16])
+        ,       TrackEnv "kickw_" (TrackCtx 0 0 256) False (RTrack (MPe "kick" (I 128)) [0,5%32,7%32,6%10,27%32])
+        --,       TrackEnv "zillUnphase" (TrackCtx 0 0 128) False (RTrack (MPh "zill" (S 4)) [0,1%3,2%3])
+        --,       TrackEnv "zillRephase" (TrackCtx 0 0 128) False (RTrack (MPh "zill" (S (-4))) [0,1%6,5%7])
          
         ] 
-        
+       
+
 
 main = do
-        (p:n:_) <- getArgs
+        (f:p:n:_) <- getArgs
         bootSynths
-        m <- mkMetronome mkTrack (RealTime $ 60/read n/read p) traks
-        run suona m 
+        tstv <- var True
+        m <- mkMetronome mkTrack (RealTime $ 60/read n/read p) []
+        ms <- newTVarIO []
+        sc <- forkIO $ run suona ms m 
+        runInputT (Settings completeFilename (Just ".supercollider2.history") True) $ 
+                let loop = do 
+                        mcom <- getInputLine "command> " 
+                        case mcom of 
+                                Nothing -> lift $ killThread sc 
+                                Just com -> const loop =<< case reads com of 
+                                        [(x,_)] -> lift . atomically . modifyTVar ms . (:) $ x
+                                        _ -> outputStrLn "no parse!"
+                in loop
  
--- System/Metronome2.hs
+++ System/Metronome2.hs
@@ -2,14 +2,14 @@
 
 module System.Metronome2 where
 
-import Control.Monad (ap)
-import Data.List (sortBy, foldl') 
+import Control.Monad (ap, forM_, foldM)
+import Data.List (sortBy, foldl',partition) 
 import Data.Ord (comparing)
 import Data.Maybe (mapMaybe)
 import Data.Ratio (Rational, (%)) 
 import Sound.OpenSoundControl (utcr, sleepThreadUntil)
 import Data.Map (insert,delete,adjust,Map,empty,keys, findWithDefault, singleton, insertWith, union)
-
+import Control.Concurrent.STM (TVar, atomically, readTVar,writeTVar)
 newtype Time = Time Integer deriving (Num, Real, Enum, Integral, Ord, Eq, Show , Read)
 newtype Phase = Phase Integer deriving (Num, Real, Enum, Integral, Ord, Eq, Show, Read)
 newtype Period = Period Integer deriving (Num, Real, Enum, Integral, Ord, Eq, Show, Read)
@@ -17,23 +17,20 @@ newtype Priority = Priority Double deriving (Num, Real, Enum, Ord, Eq, Show, Rea
 newtype RealTime = RealTime Double deriving (Num, Real, Enum, Ord, Eq, Fractional, Show,Read)
 
 
-data TrackCtx = TrackCtx {
-                priority :: Priority
-        ,       phase :: Phase
-        ,       period :: Period
-        } deriving (Show,Read)
+data TrackCtx = TrackCtx Priority Phase Period deriving (Show,Read)
+priority (TrackCtx x _ _) = x
 
-data TrackEnv t b  = TrackEnv {
-                identity :: b
-        ,       context :: TrackCtx
-        ,       mute :: Bool
-        ,       track :: t
-        } deriving (Show,Read)
+data TrackEnv t b  = TrackEnv b TrackCtx Bool t deriving (Show,Read)
 
+identifier (TrackEnv x _ _ _) = x
+context (TrackEnv _ x _ _) = x
+track (TrackEnv _ _ _ x) = x
+mute (TrackEnv _ _ x _) = x
 type Track t b a = t -> TrackCtx -> Time -> Event b a
 
-data Operation a = S a | P a | C (Operation a) (Operation a) deriving (Show,Read)
+data Operation a = I a | S a | P a | C (Operation a) (Operation a) deriving (Show,Read)
 
+I x `operate` _ = x
 S x `operate` y = x + y
 P x `operate` y = x * y
 C f g `operate` y = f `operate` (g `operate` y)
@@ -74,35 +71,79 @@ modifyShared es sh = foldl' f sh es where
         f sh _ = sh
         
 step :: forall t b a. (Eq b, Ord b) => Track t b a -> Time -> Shared b -> [TrackEnv t b]  -> ([TrackEnv t b], Shared b, [a])
-step mkTrack t sh ts = let es = nextEvents mkTrack sh ts t
-        in      (       map (modifyTrackEnv es) ts 
+step mkTrack t sh ts = digest sh ts (nextEvents mkTrack sh ts t)
+                
+
+digest :: forall t b a. (Eq b, Ord b) =>  Shared b -> [TrackEnv t b]  -> [Event b a] -> ([TrackEnv t b], Shared b, [a])
+digest sh ts es = (       map (modifyTrackEnv es) ts 
                 ,       modifyShared es sh
                 ,       mapMaybe onAction es
                 )
-        
 data Metronome t b a = Metronome {
           shared :: Shared b
         , tracks :: [TrackEnv t b]
         , actions :: [a]
         , time :: RealTime
+        , futures :: [(Time,RealTime)]
+        , messages :: [MM t b a] -> Metronome t b a
         , next :: IO (Metronome t b a)
         }
 
+data MM t b a = MME (Event b a) | AT (TrackEnv t b) | RT b | SM | RM | MTmM (Operation RealTime) | MPhM (Operation Time) 
+        | Sv FilePath | Ld FilePath | Rst [TrackEnv t b] (Shared b) deriving (Read,Show)
+
+isMM (Sv f) = True
+isMM (Ld f) = True
+isMM _ = False
 
 mkMetronome :: (Eq b, Ord b) => Track t b a -> RealTime -> [TrackEnv t b] -> IO (Metronome t b a)
-mkMetronome mkTrack (RealTime d) ts = do
-        now <- utcr
-        let loop trts (ts,sh) = do
-                        now <- utcr
+mkMetronome mkTrack d ts = do
+        let     nm sh ts as rt trts = let m = Metronome sh ts as rt trts (mm m) (loop trts (ts,sh)) in m
+
+                loop trts (ts,sh) = do
+                        now <- RealTime `fmap` utcr
                         let     ((t,rt):trts') = dropWhile ((<now). snd) $ trts
-                                (ts',sh',as) = step mkTrack (Time t) sh ts
-                        sleepThreadUntil rt
-                        return $ Metronome sh' ts' as (RealTime rt) (loop trts' (ts',sh')) 
+                                (ts',sh',as) = step mkTrack t sh ts
+                        sleepThreadUntil $ realToFrac rt
+                        return $ nm sh' ts' as rt trts'
+                mm' (Metronome sh ts as rt trts _ l) (MME e) = let
+                        (ts',sh',as') = digest sh ts [e]
+                        in nm sh' ts' (as' ++ as) rt trts
+                mm' (Metronome sh ts as rt trts _ l) (AT t) = nm sh (t:filter ((/=) (identifier t) . identifier) ts) as rt trts
+                mm' (Metronome sh ts as rt trts _ l) (RT b) = nm sh (filter ((/=) b . identifier) ts) as rt trts
+                mm' (Metronome sh ts as rt trts _ l) (MTmM (S dt)) = nm sh ts as rt trts' where
+                        trts' = zip [n ..] [t0 + dt, t1 + dt ..]
+                        (n,t0):(_,t1):_ = trts
+                mm' (Metronome sh ts as rt trts _ l) (MTmM (P dt)) = nm sh ts as rt trts' where
+                        trts' = zip [n ..] [t0 , t0 + (t1 - t0) * dt ..]
+                        (n,t0):(_,t1):_ = trts
+                mm' (Metronome sh ts as rt trts _ l) (MTmM (I t)) = nm sh ts as rt trts' where
+                        trts' = zip [n ..] [t0 , t0 + t ..]
+                        (n,t0):(_,t1):_ = trts
+                mm' (Metronome sh ts as rt trts _ l) (MPhM (S dp)) = nm sh ts as rt trts' where
+                        trts' = zip [(n + dp) ..] [t0 , t1 ..]
+                        (n,t0):(_,t1):_ = trts
+                mm' m (MPhM (P dp)) = m
+                mm'  (Metronome _ _ _ rt trts _ _) (Rst ts sh) = nm sh ts [] rt trts
+                mm m = foldl' mm' m 
+        now <- RealTime `fmap` utcr
         loop (zip [0..][now, now + d ..]) (ts, empty)
        
 
-run :: (RealTime -> Shared b -> [a] -> IO ()) -> Metronome t b a -> IO ()
-run f (Metronome sh ts as rt st) = f rt sh as >> st >>= run f 
+run :: (Show t, Show b,Ord b, Read b, Read t) => (RealTime -> Shared b -> [a] -> IO ()) -> TVar [MM t b a] -> Metronome t b a -> IO ()
+run f tvs (Metronome sh ts as rt trts mm st) = do 
+        -- play
+        f rt sh as 
+        ms <- atomically (readTVar tvs >>= \x -> writeTVar tvs [] >> return x)
+        let     (zs,ms') = partition isMM ms
+                g m (Sv n) = writeFile n (show (ts,sh)) >> return m
+                g m@(Metronome sh ts as rt trts mm l) (Ld n) = do
+                        s <- readFile n
+                        case reads s of
+                                [((ts,sh),_)] -> return (mm [Rst ts sh])
+                                _ -> putStrLn ("Parse of " ++ n ++ " failed!") >> return m
+        m' <- foldM g (mm ms') zs 
+        next m' >>=  run f tvs
 
 
 -----------------------------------------------------------------------------------------
